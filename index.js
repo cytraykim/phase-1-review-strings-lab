d7bab114eb54ef609006137823287108a4e52845
Tracy <tracy.kagiri@moringa.student.com>
2023-06-20 11:18:05 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your code in this file!
+const currentUser = 'Tracy Kagiri';
+
+const welcomeMessage = `Welcome to Flatbook, ${'Tracy Kagiri'}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${'Tracy Kagiri'.slice(0, 1)}!`;
+
+
+
+
+
+
+
+
+
+
+
+
+
